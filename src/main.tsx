e37bd8cbe3b5f48fca831a68a15542dda58dc4d8
Aladdin Alizada <tu7c5zneb@code.edu.az>
2025-06-03 02:30:22 +0400

    final

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
